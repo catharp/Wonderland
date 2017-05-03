2e2b166c12f3ce8222f91775352b9f91fdf7ecd7
Collin Tharp <catharp@gmail.com>
2017-05-02 22:19:41 -0700

    Update copy

--- src/app/index.js
+++ src/app/index.js
@@ -8,10 +8,10 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Like great food and learning new skills from real people?</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+           So do we, and we're planning something big!  Sign up, and we'll keep you posted...
         </p>
       </div>
     );
