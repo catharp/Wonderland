712e1b66dfeb994cdf0ecfe092424811525f777c
Collin Tharp <catharp@gmail.com>
2017-05-02 22:37:13 -0700

    Implement redux and router

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import reducers from './reducers';
 import App from './app';
 
+const store = createStore(reducers);
+
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <Router>
+      <Route path="/" component={App} />
+    </Router>
+  </Provider>,
   document.getElementById('root')
 );
