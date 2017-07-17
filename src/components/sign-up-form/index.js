44f186fdc624b4fb0ebfdddef8c3a52881a029f4
Collin Tharp <catharp@gmail.com>
2017-07-16 19:59:40 -0700

    Lint

--- src/components/sign-up-form/index.js
+++ src/components/sign-up-form/index.js
@@ -1,33 +1,24 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Field, reduxForm } from 'redux-form';
 
-const postToSignUp = (body) => {
-  console.log('posting:', body);
-  fetch("/signup",
-  {
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      },
-      method: "POST",
-      body: JSON.stringify(body)
+const postToSignUp = info =>
+  fetch('/signup', {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(info),
   });
-}
 
-class SignUpForm extends Component {
-  render() {
-    const { handleSubmit } = this.props;
-    return (
-      <form onSubmit={handleSubmit(postToSignUp)}>
-        <Field name='email' component='input' type='text' />
-        <button type='submit'>Sign Up!</button>
-      </form>
-    );
-  }
+const SignUpForm = () => {
+  const { handleSubmit } = this.props;
+  return (
+    <form onSubmit={handleSubmit(postToSignUp)}>
+      <Field name="email" component="input" type="text" />
+      <button type="submit">Sign Up!</button>
+    </form>
+  );
 };
 
-SignUpForm = reduxForm({
-  form: 'signUp'
-})(SignUpForm);
-
-export default SignUpForm;
+export default reduxForm({ form: 'signUp' })(SignUpForm);
